7dc7a3aee513a0c904361e23c4ddbb6d2c722cd1
masumali23020 <mdmasum0218@gmail.com>
2024-03-04 15:53:30 +0600

    In the fourth part of the FaceHook project, we'll focus on implementing JSON Web Token (JWT) authentication to enhance the security of our application.

--- src/common/Header.jsx
+++ src/common/Header.jsx
@@ -5,8 +5,10 @@ import notification from "../assets/icons/notification.svg"
 import avatar from "../assets/images/avatars/avatar_1.png"
 import logo from "../assets/images/logo.svg"
 import LogOut from '../components/Auth/LogOut'
+import { useAuth } from '../hooks/useAuth'
 
 const Header = () => {
+  const {auth} = useAuth()
   return (
     <nav className="sticky top-0 z-50 border-b border-[#3F3F3F] bg-[#1E1F24] py-4">
     <div className="container flex flex-col items-center justify-between gap-6 sm:flex-row">
@@ -27,7 +29,7 @@ const Header = () => {
         <LogOut />
 
         <button className="flex-center !ml-8 gap-3">
-          <span className="text-lg font-medium lg:text-xl">Sumit</span>
+          <span className="text-lg font-medium lg:text-xl">{auth?.user?.firstName}</span>
           <img className="max-h-[32px] max-w-[32px] lg:max-h-[44px] lg:max-w-[44px]"
             src={avatar} alt="avatar" />
         </button>
